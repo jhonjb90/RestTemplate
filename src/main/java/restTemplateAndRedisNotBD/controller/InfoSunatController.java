9ffe786a0f06910c0472eabe2bbccf92c154ceaa
Jhon Inofuente <jhonjb90@gmail.com>
2024-11-17 18:57:55 -0500

    haciendo el controller

---
+++ src/main/java/restTemplateAndRedisNotBD/controller/InfoSunatController.java
@@ -0,0 +1,27 @@
+package restTemplateAndRedisNotBD.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import restTemplateAndRedisNotBD.aggregates.response.ResponseSunat;
+import restTemplateAndRedisNotBD.service.InfoSunatService;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/v1/restTemplate")
+@RequiredArgsConstructor
+public class InfoSunatController {
+    private final InfoSunatService infoSunatService;
+
+    @GetMapping("/infoSunat")
+    public ResponseEntity<ResponseSunat> getInfoSunat(
+            @RequestParam("ruc") String ruc) throws IOException{
+        return new ResponseEntity<>(infoSunatService.getInfoSunat(ruc), HttpStatus.OK);
+    }
+    )
+}
