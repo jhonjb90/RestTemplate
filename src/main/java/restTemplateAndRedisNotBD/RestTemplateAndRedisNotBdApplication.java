6757f82e4e9f8c05ca5fe1035722a1c2e9a46428
Jhon Inofuente <jhonjb90@gmail.com>
2024-11-17 15:49:06 -0500

    iniciando proyecto

---
+++ src/main/java/restTemplateAndRedisNotBD/RestTemplateAndRedisNotBdApplication.java
@@ -0,0 +1,13 @@
+package restTemplateAndRedisNotBD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTemplateAndRedisNotBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTemplateAndRedisNotBdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/restTemplateAndRedisNotBD/RestTemplateAndRedisNotBdApplicationTests.java
@@ -0,0 +1,13 @@
+package restTemplateAndRedisNotBD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTemplateAndRedisNotBdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
