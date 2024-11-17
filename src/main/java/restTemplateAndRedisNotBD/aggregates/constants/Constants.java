90f0ef0d3cff9d25d5c33d3aaa5e6de16296e880
Jhon Inofuente <jhonjb90@gmail.com>
2024-11-17 16:01:55 -0500

    creando nuestra constante, response, config

---
+++ src/main/java/restTemplateAndRedisNotBD/aggregates/constants/Constants.java
@@ -0,0 +1,10 @@
+package restTemplateAndRedisNotBD.aggregates.constants;
+
+public class Constants {
+    public static final String USER_CREATED = "JINOFUENTE";
+    public static final String BEABER = "Bearer ";
+    public static final int ESTADO_ACTIVO = 1;
+    public static final String BASE_URL = "https://api.apis.net.pe";
+    public static final int REDIS_TTL = 5;
+    public static final String REDIS_KEY_API_RENIEC = "MS:REGISTRO:CONSULTA:SUNAT";
+}
